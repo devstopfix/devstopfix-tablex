54d05df4984094c03cb726c813b449a6260e34e9
J Every <devstopfix@gmail.com>
2023-06-06 22:24:27 +0100

    chore: use nimble example for date

--- lib/tablex/parser/expression/date.ex
+++ lib/tablex/parser/expression/date.ex
@@ -2,23 +2,19 @@ defmodule Tablex.Parser.Expression.Date do
   @moduledoc false
 
   import NimbleParsec
-  import Tablex.Parser.Space, only: [eow: 0]
 
   def date do
-    ascii_string([?0..?9], 4)
-    |> string("-")
-    |> ascii_string([?0..?1], 1)
-    |> ascii_string([?0..?9], 1)
-    |> string("-")
-    |> ascii_string([?0..?3], 1)
-    |> ascii_string([?0..?9], 1)
-    |> lookahead(eow())
+    integer(4)
+    |> ignore(string("-"))
+    |> integer(2)
+    |> ignore(string("-"))
+    |> integer(2)
     |> reduce({__MODULE__, :trans_date, []})
   end
 
   @doc false
-  def trans_date(parts) do
-    parts |> to_string() |> Date.from_iso8601!()
+  def trans_date([yyyy, mm, dd]) do
+    Date.new!(yyyy, mm, dd)
   end
 
 end
